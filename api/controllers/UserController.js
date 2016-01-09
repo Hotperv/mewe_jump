bcacb69ac2adcfb6da61ffa0d9af2bb59460e8cf
Jeff Robert Dagala <jeff@drumbi.com>
2016-01-09 16:12:30 +0800

    Login

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -16,7 +16,9 @@ module.exports = {
 				}
 				else
 				{
-					User.create(req.body, function(errCreate, user) {
+					var tempUser = req.body;
+					tempUser.email = tempUser.email.toLowerCase();
+					User.create(tempUser, function(errCreate, user) {
 						return res.send(200, {success:true, user: user})
 					})
 				}
@@ -30,6 +32,34 @@ module.exports = {
 	},
 	login: function(req, res) {
 		return res.view({ title: "Login | MeWe Jump"});
+	},
+	processLogin: function(req, res) {
+		if(req.param("email") && req.param("password"))
+		{
+			User.findOne({email: req.param("email").toLowerCase()}, function(err, user){
+				if(user && user.email && user.email.toLowerCase() == req.param("email").toLowerCase() && user.password && user.password == req.param("password") )
+				{
+					var tempUser = _.clone(user);
+					tempUser.login = true;
+					console.log(JSON.stringify(tempUser, null, 4))
+					res.cookie('meweauth', tempUser, { maxAge: 7776000000 });
+					// return res.redirect("/user/registration");
+					return res.send(200, {success:true, message: "Email and password matched!.", redirect: "/home"})
+				}
+				else if(user && user.email && user.email.toLowerCase() == req.param("email").toLowerCase())
+				{
+					return res.send(200, {success:false, message: "Incorrect email and password combination."})
+				}
+				else
+				{
+					return res.send(200, {success:false, message: "User does not exist."})
+				}
+			})
+		}
+		else
+		{
+			return res.send(200, {success:false, message: "Email and Password are required."})
+		}
 	},
 	registration: function(req,res) {
 		return res.view({ title: "Registraiton | MeWe Jump"});
